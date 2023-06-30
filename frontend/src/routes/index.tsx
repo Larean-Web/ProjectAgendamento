5050ce21b649c20af39fd5aa3a8e48a6ccd0319f
Otavio <octtavo9@gmail.com>
2023-06-30 11:06:14 -0300

    verificando os inputs

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SigIn from "../pages/signin";
-import SigUp from "../pages/signup";
-import NewPassword from "../pages/newpasswordscrenn";
-import RedefinePassword from "../pages/redefinepasswordscreen";
+import SigIn from "../pages/login";
+import SigUp from "../pages/registro";
+import NewPassword from "../pages/novaSenha";
+import RedefinePassword from "../pages/redefinirSenha";
 
 function RouterApp() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SigIn />} />
-        <Route path="/register" element={<SigUp />} />
-        <Route path="/newpassword" element={<NewPassword />} />
-        <Route path="/redefinepassword" element={<RedefinePassword/>} />
+        <Route path="/registro" element={<SigUp />} />
+        <Route path="/novasenha" element={<NewPassword />} />
+        <Route path="/redefinirsenha" element={<RedefinePassword/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/services/conexoes.tsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_APP_API_KEY
+})  
+
+export default api
